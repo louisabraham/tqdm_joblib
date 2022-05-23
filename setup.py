6f30f5bcb3e55ff122eb7fc30e0fd8e51ef45ff2
Louis Abraham <louis.abraham@yahoo.fr>
2022-05-23 10:52:13 +0200

    fix

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 
 setup(
     name="tqdm_joblib",
-    version="0.0.1",
+    version="0.0.2",
     author="Louis Abraham",
     author_email="louis.abraham@yahoo.fr",
     license="CC BY-SA 4.0",
     description="Tracking progress of joblib.Parallel execution",
     url="https://stackoverflow.com/a/58936697/5133167",
+    packages=["tqdm_joblib"],
     install_requires=[
         "tqdm",
     ],
